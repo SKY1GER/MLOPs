0f8e432021d44de7853389cb815461dc493a41f8
SKY1GER <saikumarb9119@gmail.com>
2026-07-05 18:37:30 +0530

    MLOPS practice

--- Intent-classifier-model/docker.sh
+++ Intent-classifier-model/docker.sh
@@ -1,7 +1,12 @@
-# docker
-yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
+#!/bin/bash
+set -euxo pipefail
+
+dnf install -y dnf-plugins-core
+
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl enable --now docker
+
 usermod -aG docker ec2-user
